401e0057842d785527f7d3b7479069b1cc988972
fmesa1213 <52666235+fmesa1213@users.noreply.github.com>
2023-02-28 22:59:10 -0500

    Create upper.cpp

---
+++ upper.cpp
@@ -0,0 +1,30 @@
+/*
+Author: Frida Mesa
+Course: CSCI-135
+Instructor: 
+Assignment: Lab4E
+Wrote a program that prints the top-right half of a square, given the side length.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   int length;
+
+        cout << "Input side length: ";
+        cin >> length;
+        
+cout << "Shape: " << endl;
+        for(c = 0; c < length; c++) {
+                for(j=0; j < c; j++) {
+                        cout << " ";
+                }
+                for(j=0; j<(length - c); j++) {
+                        cout << "*";
+                }
+                cout << endl;
+          return 0;
+  
+}
